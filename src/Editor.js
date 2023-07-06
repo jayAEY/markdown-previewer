62944b7f34fbd7d82ecbdcd64ebcd1390eb397da
jayAEY <106685702+jayAEY@users.noreply.github.com>
2023-07-06 13:50:29 -0700

    first commit

---
+++ src/Editor.js
@@ -0,0 +1,16 @@
+// import React, {useState} from 'react'
+
+// const Editor = () => {
+//   const [text, setText] = useState('asd')
+  
+//   const handleChange = (event) =>{
+//     setText(event.target.value)
+//   }
+
+
+//   return (
+//     <textarea id='editor' onChange={handleChange} value={text}/>
+//   )
+// }
+
+// export default Editor
---
+++ src/Preview.js
@@ -0,0 +1,13 @@
+// import React, {useState} from 'react'
+// import Editor from './Editor'
+
+
+// const Preview = () => {
+//  let change = 'sdf'
+    
+//   return (
+//     <div id="preview">{change}</div>
+//   )
+// }
+
+// export default Preview
